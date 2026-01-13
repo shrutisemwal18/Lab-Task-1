56f591adbb404c7fd68f2351cececbdc1190abb5
shrutisemwal18 <semwalshruti43@gmail.com>
2026-01-13 19:20:02 +0530

    Add diamond pattern printing program
    This program prints a diamond shape pattern using asterisks.

---
+++ program4_diamond.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+
+    // Upper half
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++)
+            printf(" ");
+        for (int j = 1; j <= (2 * i - 1); j++)
+            printf("*");
+        printf("\n");
+    }
+
+    // Lower half
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--)
+            printf(" ");
+        for (int j = 1; j <= (2 * i - 1); j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
